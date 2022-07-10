9a1b6e5a7e0e5d353485cf54ec9928f8f87833c4
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-07-10 23:22:33 +0530

    CPP

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	int a[n];
+	cin>>n;
+	for(int i=1; i<=n; i++)
+	{
+		cin>>a[i];
+	}
+	return 0;
+}
