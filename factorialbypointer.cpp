1a7dc9391ef4f63795ead1020598e7686c0146ec
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-08-19 16:33:59 +0530

    CPP

---
+++ factorialbypointer.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,n, fact=1;
+    int *p;
+    p=&fact;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        *p=*p*i;
+    }
+    cout<<*p;
+    return 0;
+}
