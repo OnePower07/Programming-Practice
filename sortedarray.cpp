89bd28c187b95a3fe1895368c13e2ae268dbfeeb
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-09-05 16:14:05 +0530

    CPP

--- sortedarray.cpp
+++ sortedarray.cpp
@@ -1,21 +1,50 @@
 #include<iostream>
 using namespace std;
-
 int main()
 {
-    int A[5]={0,1,2,0,2},temp;
+    int A[5]={0,1,0,1,2},a=0,b=0,c=0;
+    int arr[5];
     int i=0;
-    int j=4;
-    for(i=0;i<5;i++)
+    int j=0;
+    for(int i=0;i<5;i++)
     {
-        for(j=0;j<5;j++){
-        if(A[j]>A[i])
+        if(A[i]==0)
+        a=0;
+    }
+    for(int i=0;i<5;i++)
+    {
+        if(A[i]==1)
+        b=1;
+    }
+    for(int i=0;i<5;i++)
+    {
+        if(A[i]==2)
+        c=2;
+    }
+    for(int i=0;i<5;i++)
+    {
+        if(A[i]==a)
         {
-            temp=A[i];
-            A[i]=A[j];
-            A[j]=temp;
+            arr[j++]=A[i];
         }
+    }
+    for(int i=0;i<5;i++)
+    {
+        if(A[i]==b)
+        {
+            arr[j++]=A[i];
         }
+    }
+    for(int i=0;i<5;i++)
+    {
+        if(A[i]==c)
+        {
+            arr[j++]=A[i];
+        }
+    }
+    for(int i=0,j=0;i<5 && j<5;i++,j++)
+    {
+        A[i]=arr[j];
     }
     for(i=0;i<5;i++)
     {
