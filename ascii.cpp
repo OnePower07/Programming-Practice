b20ac89e0e81e7ce2991da0fe0717616b6c47bc7
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-09-27 20:18:22 +0530

    CPP

---
+++ ascii.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    for(int i=0;i<=100;i++)
+    {
+        cout << "The ASCII code of "<<char(i)<<" is: "<<i<<endl;
+    }
+    return 0;
+}
